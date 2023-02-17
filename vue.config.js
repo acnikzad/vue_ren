8e75c2d52546941f8fee729dfc88d6320a7b6f1e
Aria Nikzad <a.c.nikzad@gmail.com>
2023-02-16 16:30:27 -0800

    fixing bug

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath: process.env.BASE_URL,
+  // NODE_ENV: '"development"',
+  // VUE_APP_BASE_URL: '"https://renhousingsolutions.com/"',
   transpileDependencies: true
 })
